064388afbdf4bc436c60f0e9895c546090443643
Nate Stewart <nate@cockroachlabs.com>
2018-01-24 07:20:17 -0500

    added attrs

--- scripts/update-crdb-statefulset.py
+++ scripts/update-crdb-statefulset.py
@@ -5,7 +5,8 @@ parser = argparse.ArgumentParser(description='Update statefulset yaml with user-
 parser.add_argument('config_yaml', metavar="config_file_url", help='url to yaml file containing cockroachdb statefulset configuration')
 parser.add_argument('--size',  dest="size_in_gb", type=int, help='Desired volume size in GiB')
 parser.add_argument('--version-tag', dest="version_tag", help='CockroachDB Docker tag to use')
-parser.add_argument('--locality', dest="locality", help='Locality arguments to use')
+parser.add_argument('--locality', dest="locality", required=True, help='Locality argument to use')
+parser.add_argument('--attrs', dest="attrs", required=True, help='Attribute argument to use')
 
 args = parser.parse_args()
 
@@ -18,16 +19,16 @@ for config in configs:
         if args.size_in_gb:
             config['spec']['volumeClaimTemplates'][0]['spec']['resources']['requests']['storage'] = str(args.size_in_gb) + 'Gi'
 
-        if args.locality:
-            for container in config['spec']['template']['spec']['containers']:
-                if container['name'] == 'cockroachdb':
-                    container['command'] = [
-                        "/bin/bash",
-                        "-ecx",
-                        "exec /cockroach/cockroach start --logtostderr --insecure --host $(hostname -f) --locality=" +
-                            args.locality +" --http-host 0.0.0.0 --join cockroachdb-0.cockroachdb,cockroachdb-1.cockroachdb,cockroachdb-2.cockroachdb --cache 25% --max-sql-memory 25%"
-                    ]
-                    break
+
+        for container in config['spec']['template']['spec']['containers']:
+            if container['name'] == 'cockroachdb':
+                container['command'] = [
+                    "/bin/bash",
+                    "-ecx",
+                    "exec /cockroach/cockroach start --logtostderr --insecure --host $(hostname -f) --locality=" +
+                        args.locality +" --http-host 0.0.0.0 --attrs=" + args.attrs + " --join cockroachdb-0.cockroachdb,cockroachdb-1.cockroachdb,cockroachdb-2.cockroachdb --cache 25% --max-sql-memory 25%"
+                ]
+                break
 
 
         if args.version_tag:
