9eb865a228f569b9c70efaa179104f2dd7f86cb8
Nate <nathaniel.p.stewart@gmail.com>
2018-01-29 10:18:59 -0500

    Add localities, attributes, and locations to clusters (#4)
    Automatically tag nodes with a locality and attribute signifying the AWS region/az pair and ec2 instance type, respectively.
    
    If this is a 2.x cluster, the template will insert lat/long values for the region into the system.locations table so the cluster can be plotted on a map.

--- scripts/update-crdb-statefulset.py
+++ scripts/update-crdb-statefulset.py
@@ -5,6 +5,8 @@ parser = argparse.ArgumentParser(description='Update statefulset yaml with user-
 parser.add_argument('config_yaml', metavar="config_file_url", help='url to yaml file containing cockroachdb statefulset configuration')
 parser.add_argument('--size',  dest="size_in_gb", type=int, help='Desired volume size in GiB')
 parser.add_argument('--version-tag', dest="version_tag", help='CockroachDB Docker tag to use')
+parser.add_argument('--locality', dest="locality", required=True, help='Locality argument to use')
+parser.add_argument('--attrs', dest="attrs", required=True, help='Attribute argument to use')
 
 args = parser.parse_args()
 
@@ -16,6 +18,18 @@ for config in configs:
     if config['kind'] == 'StatefulSet':
         if args.size_in_gb:
             config['spec']['volumeClaimTemplates'][0]['spec']['resources']['requests']['storage'] = str(args.size_in_gb) + 'Gi'
+
+
+        for container in config['spec']['template']['spec']['containers']:
+            if container['name'] == 'cockroachdb':
+                container['command'] = [
+                    "/bin/bash",
+                    "-ecx",
+                    "exec /cockroach/cockroach start --logtostderr --insecure --host $(hostname -f) --locality=" +
+                        args.locality +" --http-host 0.0.0.0 --attrs=" + args.attrs + " --join cockroachdb-0.cockroachdb,cockroachdb-1.cockroachdb,cockroachdb-2.cockroachdb --cache 25% --max-sql-memory 25%"
+                ]
+                break
+
 
         if args.version_tag:
             for container in config['spec']['template']['spec']['containers']:
